9560085d00f484eb729d273bfb150947c08e3bd2
Ben Andrews <bandrews@mofangheavyindustries.com>
2015-07-06 14:24:56 -0700

    Missing adds for Abyss support

---
+++ arduino/altar/Abyss.h
@@ -0,0 +1,51 @@
+#ifndef Abyss_h
+#define Abyss_h
+
+#include "Arduino.h"
+
+// ***********************************
+// IAbyssHandler interface.
+// ***********************************
+class IAbyssHandler
+{
+    public:
+        virtual ~IAbyssHandler() {}
+        virtual void xbee_println(char* message) = 0;
+        virtual bool xbee_available() = 0;
+        virtual char xbee_read() = 0;
+        virtual void received_message(char* message) = 0;
+};
+
+// ***********************************
+// Abyss class.
+// Handles all communication between the arduino and Abyss.
+// ***********************************
+class Abyss
+{
+    public:
+        Abyss(const char* id, IAbyssHandler* handler);
+        void check_xbee_messages();
+        void send_message(const char* message);
+        
+        // string helpers
+        static int string_index_of(char* text, const char* substring);
+        static bool string_contains(char* text, const char* substring);
+        static bool string_compare(const char* a, const char* b);
+    
+    private:
+        const char* DELIMITER = "::";
+        const char* my_id;
+        IAbyssHandler* my_handler;
+        char head[32];
+        char body[256];
+        char receive_buffer[256];
+        char send_buffer[256];
+        int buffer_index = 0;
+        bool check_message;
+        
+        void split_buffer_into_head_and_body();
+        void clear_buffers();
+};
+
+#endif
+
---
+++ arduino/altar/abysshandler.h
@@ -0,0 +1,66 @@
+#ifndef abysshandler_h
+#define abysshandler_h
+
+#include "abyss.h"
+
+#define XBEE_RX 3
+#define XBEE_TX 2
+#include <SoftwareSerial.h>
+SoftwareSerial XBee(XBEE_RX, XBEE_TX); // RX, TX
+
+class AbyssHandler : public IAbyssHandler
+{
+  private:
+  
+    bool isInitialized = false;
+    public:        
+    
+        // send a message over the desired serial 
+        // connection ending with a newline
+        virtual void xbee_println(char* message)
+        {
+          if (!isInitialized)
+          {
+            XBee.begin(9600);
+          }
+            XBee.println(message);
+        }
+        
+        // tests if any unread data is available 
+        // from the serial receive
+        virtual bool xbee_available()
+        {
+          if (!isInitialized)
+          {
+            XBee.begin(9600);
+          }
+            return XBee.available();
+        }
+        
+        // reads one byte of data from the serial connection
+        virtual char xbee_read()
+        {
+          if (!isInitialized)
+          {
+            XBee.begin(9600);
+          }
+            return XBee.read();
+        }
+        
+        // this is called when a message is sent 
+        // from Abyss to this unique arduino.
+        virtual void received_message(char* message)
+        {
+        }
+};
+
+// ***********************************
+// Create Abyss and handler objects.
+// Define a unique ID.
+// This arduino will only respond to messages that are sent with a header matching this ID.
+// ***********************************
+const char* MY_ID = "ALTAR";
+AbyssHandler* handler = new AbyssHandler();
+Abyss* abyss = new Abyss(MY_ID, handler);
+
+#endif
